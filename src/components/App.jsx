5d1ed9237b7d0505254bc9309256cd7c6cd70a97
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-03-22 23:01:42 +0100

    сhanged classes to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,121 +1,105 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import Modal from './Modal/Modal';
 import { Button } from '../components/ContactList/ContactList.styled';
 import { nanoid } from 'nanoid';
-
 import { PropTypes } from 'prop-types';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-    showModal: false,
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
+export default function App() {
+  const [contacts, setContacts] = useState(() => {
+    const contacts = window.localStorage.getItem('contacts');
     const parsed = JSON.parse(contacts);
+    return (
+      parsed || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
-    if (parsed) {
-      this.setState({ contacts: parsed });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts)
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  formSubmitHandler = ({ name, number }) => {
+  const formSubmitHandler = ({ name, number }) => {
     const newContact = {
       id: nanoid(),
       name,
       number,
     };
 
-    const checkName = this.state.contacts.some(contact =>
+    const checkName = contacts.some(contact =>
       contact.name.toLowerCase().includes(name.toLowerCase())
     );
-
+    console.log(checkName);
     if (!checkName) {
-      this.setState(prevState => ({
-        contacts: [newContact, ...prevState.contacts],
-      }));
+      setContacts([...contacts, newContact]);
     } else {
       alert(`${name}is already in contacts.`);
     }
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const togleModal = () => {
+    setShowModal(!showModal);
   };
 
-  togleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  render() {
-    const filterNormilized = this.state.filter.toLowerCase().trim();
-    const visibleContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterNormilized)
-    );
+  const filterNormilized = filter.toLowerCase().trim();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filterNormilized)
+  );
 
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
 
-        <Button
-          type="button"
-          style={{
-            marginTop: '10px',
-          }}
-          onClick={this.togleModal}
-        >
-          Contacts
-        </Button>
-        {this.state.showModal && (
-          <Modal onClick={this.togleModal}>
-            <h2
-              style={{
-                textAlign: 'center',
-                paddingBottom: '30px',
-              }}
-            >
-              Contacts
-            </h2>
-            <Filter value={this.state.filter} onChange={this.changeFilter} />
-            <ContactList
-              contacts={visibleContacts}
-              onDeleteContact={this.deleteContact}
-            />
+      <Button
+        type="button"
+        style={{
+          marginTop: '10px',
+        }}
+        onClick={togleModal}
+      >
+        Contacts
+      </Button>
 
-            <Button type="button" onClick={this.togleModal}>
-              Close
-            </Button>
-          </Modal>
-        )}
-      </div>
-    );
-  }
+      {showModal && (
+        <Modal onClick={togleModal}>
+          <h2
+            style={{
+              textAlign: 'center',
+              paddingBottom: '30px',
+            }}
+          >
+            Contacts
+          </h2>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={deleteContact}
+          />
+
+          <Button type="button" onClick={togleModal}>
+            Close
+          </Button>
+        </Modal>
+      )}
+    </div>
+  );
 }
-
-export default App;
 
 App.propTypes = {
   contacts: PropTypes.arrayOf(
